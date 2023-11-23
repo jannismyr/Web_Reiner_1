02836060d0c9cd51fb3b98e043b268204e20c580
Jannis <87652452+jannismyr@users.noreply.github.com>
2023-11-23 16:16:12 +0100

    Back End overhaul
    Post und Delete Methode für ID

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,14 +1,37 @@
 import express from 'express';
-import {Veranstaltungen} from './temp-data.js'
+import {AlleVeranstaltungen as AlleVeranstaltungenRaw} from './temp-data.js'
+
+let AlleVeranstaltungen = AlleVeranstaltungenRaw;
 
 const app = express ();
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
     res.send('Hello!');
 });
-app.get('/Veranstaltungen', (req, res) => {
-    res.json(Veranstaltungen);
+app.get('/veranstaltungen', (req, res) => {
+    res.json(AlleVeranstaltungen);
 });
+
+app.get('/veranstaltungen/:veranstaltungId', (req, res) => {
+    const veranstaltungId = req.params.veranstaltungId;
+    const veranstaltung = AlleVeranstaltungen.find(veranstaltung => veranstaltung.id === veranstaltungId);
+    res.json(veranstaltung);
+});
+
+app.post('/veranstaltungen', (req, res) => {
+   const veranstaltungId = req.body.id;
+   const NewVeranstaltung  = AlleVeranstaltungen.find(veranstaltung => veranstaltung.id === veranstaltungId);
+   AlleVeranstaltungen.push(NewVeranstaltung);
+   res.json(AlleVeranstaltungen)
+});
+
+app.delete('/veranstaltungen/:veranstaltungId', (req, res) => {
+    const veranstaltungId = req.params.veranstaltungId;
+    AlleVeranstaltungen = AlleVeranstaltungen.filter(veranstaltung => veranstaltung.id !== veranstaltungId);
+    res.json(AlleVeranstaltungen)
+})
+
 app.listen(8000, ()=> {
     console.log('Server is listening on port 8000')
 });
--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -1,16 +1,17 @@
-export const Veranstaltungen = [{
+export const AlleVeranstaltungen = [{
+    id: '1',
     veranstaltungsname: 'Heidenheimer Weihnachtsmarkt',
     datum: '02.02.2022',
     ort: 'Brunnenplatz',
     preis: '0,00€',
     genehmigung: true,
 }, {
-    id: '456',
+    id: '2',
     name: 'Rummel',
     ort: 'Rummelplatz',
     genehmigung: true,
 }, {
-    id: '789',
+    id: '3',
     name: 'Felix Rave',
     ort: 'WG Schnaitheimer Strasse',
     genehmigung: false,
