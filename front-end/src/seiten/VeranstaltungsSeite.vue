b15acbdf036e59f024ec13b2fc33566f4b238f0a
Jannis <87652452+jannismyr@users.noreply.github.com>
2023-12-04 09:39:47 +0100

    Einbindung Suche

--- front-end/src/seiten/VeranstaltungsSeite.vue
+++ front-end/src/seiten/VeranstaltungsSeite.vue
@@ -8,31 +8,51 @@
   <button class="custom-button">Gewählte Veranstaltung löschen</button>
 
     <div>
-      <form class="search-form">
-    <input type="text" class="search-input" placeholder="Suche...">
+     <form class="search-form" >
+       <input type="text" class="search-input" v-model="searchTerm" placeholder="Suche...">
     <button type="submit" class="search-button">Suche</button>
   </form>
   </div>
 </div>
   
-<div>
-  <VeranstaltungDetail v-for="Veranstaltung in AlleVeranstaltungen"
-  :key="Veranstaltung.id"
-  :Name="Veranstaltung.name"
-  :Datum="Veranstaltung.datum"
-  :Ort="Veranstaltung.ort"
-  :Preis="Veranstaltung.preis"
-  :Beschreibung="Veranstaltung.beschreibung"
-  :Genehmigung="Veranstaltung.genehmigung"
-  />
-<br>
-</div>
+<div v-if="searchTerm === ''">
+    <div>
+      <VeranstaltungDetail v-for="Veranstaltung in AlleVeranstaltungen"
+        :key="Veranstaltung.id"
+        :Name="Veranstaltung.name"
+        :Datum="Veranstaltung.datum"
+        :Ort="Veranstaltung.ort"
+        :Preis="Veranstaltung.preis"
+        :Beschreibung="Veranstaltung.beschreibung"
+        :Genehmigung="Veranstaltung.genehmigung"
+      />
+      <br>
+    </div>
+  </div>
+
+  <div v-else>
+    <div v-if="filteredVeranstaltungen.length > 0">
+      <VeranstaltungDetail v-for="Veranstaltung in filteredVeranstaltungen"
+        :key="Veranstaltung.id"
+        :Name="Veranstaltung.name"
+        :Datum="Veranstaltung.datum"
+        :Ort="Veranstaltung.ort"
+        :Preis="Veranstaltung.preis"
+        :Beschreibung="Veranstaltung.beschreibung"
+        :Genehmigung="Veranstaltung.genehmigung"
+      />
+      <br>
+    </div>
+    <div v-else>
+      <p>Keine Veranstaltung gefunden.</p>
+    </div>
+  </div>
+  
 </template>
 
 <script>
 
 import VeranstaltungDetail from '../components/VeranstaltungDetail.vue'
-
 import axios from 'axios';
 
 export default {
@@ -43,13 +63,25 @@ export default {
   data(){
     return{
       AlleVeranstaltungen: [],
+      searchTerm: '',
     }
   },
+  computed: {
+    filteredVeranstaltungen() {
+      const searchTermLowerCase = this.searchTerm.toLowerCase();
+      return this.AlleVeranstaltungen.filter(veranstaltung =>
+        veranstaltung.name.toLowerCase().includes(this.searchTerm.toLowerCase())||
+      veranstaltung.ort.toLowerCase().includes(searchTermLowerCase)
+      );
+    }
+  },
+
   async created() {
          const response = await axios.get('/api/veranstaltungen');
          const AlleVeranstaltungen = response.data;
          this.AlleVeranstaltungen = AlleVeranstaltungen;
         }
+
 }
 
 </script>
