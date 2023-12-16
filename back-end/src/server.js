70847c2196d43cc6d8f534546825b494b0a1d797
FassbinderK <116159998+FassbinderK@users.noreply.github.com>
2023-12-16 13:52:49 +0100

    Update server.js
    Apis vervollständigt.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -7,48 +7,97 @@ let AlleVeranstaltungen = AlleVeranstaltungenRaw;
 const app = express ();
 app.use(express.json());
 
+
+// Test, "historischer Codeschnipsel"
 app.get('/hello', (req, res) => {
     res.send('Hello!');
 });
+
+// Api für alle Veranstaltungen
 app.get('/api/veranstaltungen', (req, res) => {
     res.json(AlleVeranstaltungen);
 });
+// Api für nicht genehmigte Veranstaltungen
+app.get('/api/veranstaltungen/nicht-genehmigt', (req, res) => {
+    const veranstaltung = AlleVeranstaltungen.filter(veranstaltung => veranstaltung.genehmigung === false);
+    res.json(veranstaltung)
+})
+// Api für genehmigte Veranstaltungen
+app.get('/api/veranstaltungen/genehmigt', (req, res) => {
+    const veranstaltung = AlleVeranstaltungen.filter(veranstaltung => veranstaltung.genehmigung === true);
+    res.json(veranstaltung)
+})
 
+// Api für bestimmte Veranstaltung
 app.get('/api/veranstaltungen/:veranstaltungId', (req, res) => {
     const veranstaltungId = req.params.veranstaltungId;
     const veranstaltung = AlleVeranstaltungen.find(veranstaltung => veranstaltung.id === veranstaltungId);
-    res.json(veranstaltung);
+    
+
+    switch (true) {
+        case veranstaltung !== undefined:
+            res.status(200).json(veranstaltung);
+            break;
+        case veranstaltung === undefined:
+            res.status(404).json({ error: '404: Keine Veranstaltung gefunden' });
+            break;
+        default:
+            res.status(500).json({ error: '500: Interner Serverfehler' });
+    } //HTTP 200, 404 und 500
 });
 
-app.post('/api/veranstaltungen', (req, res) => {
 
-    console.log('Vor dem Hinzufügen', AlleVeranstaltungen);
-    
-    const neueVeranstaltung = {                                                             // Eingabeformat: JSON
-       id: NeueVeranstaltungsId(AlleVeranstaltungen).toString(),                            // "id": wird generiert (höchste vorhandene + 1),    
-       name: req.body.name,                                                                 // "name": "Meine Veranstaltung",
-       datum: req.body.datum,                                                               // "datum": "TT.MM.JJJJ",
-       ort: req.body.ort,                                                                   // "ort": "ein Ort"
-       preis: req.body.preis,                                                               // "preis": 12.34
-       beschreibung: req.body.beschreibung,                                                 // "beschreiung": "eine beschreibung"
-       genehmigung: req.body.genehmigung,                                                   // "genehmigung": true/false abs
-          };
- 
-    AlleVeranstaltungen.push(neueVeranstaltung);
+// Neue Veranstaltung erstellen
+app.post('/api/veranstaltungen', (req, res) => {                                            //Übergabeformat:
+    try {                                                                                   //JSON
+        const neueVeranstaltung = {                                                         // {
+            id: NeueVeranstaltungsId(AlleVeranstaltungen).toString(),                       //
+            name: req.body.name,                                                            // "name": "ein Name",
+            datum: req.body.datum,                                                          // "datum": "TT.MM.JJJJ",
+            ort: req.body.ort,                                                              // "ort": "ein Ort",
+            preis: req.body.preis,                                                          // "preis": "1.23"
+            beschreibung: req.body.beschreibung,                                            // "beschreibung": "eine Beschreibung"
+            genehmigung: req.body.genehmigung || false,                                     // "genehmigung": wenn nicht anders angegeben: false
+        };                                                                                  // }
 
-    console.log('Nach dem Hinzufügen', AlleVeranstaltungen);
+        AlleVeranstaltungen.push(neueVeranstaltung);
 
-    res.json(AlleVeranstaltungen);
- });
- 
- 
+        res.status(201).json(AlleVeranstaltungen);
+    } catch (error) {
+        // Fehler beim Parsen des JSON-Objekts
+        console.error('Fehler beim Parsen des JSON-Objekts:', error);
+        res.status(400).json({ error: 'Ungültiges JSON-Format' });
+    }
+});
+
+// Suche für Genehmigte Veranstaltungen
+app.get('/api/suche', (req, res) => {
+    const { stichwort } = req.query;
+    const veranstaltung = AlleVeranstaltungen.filter(veranstaltung => veranstaltung.genehmigung === true);
+    if (!stichwort) {
+        return res.status(400).json({ error: 'Suchbegriff fehlt' });
+    }
+
+    const gefundeneVeranstaltungen = veranstaltung.filter(veranstaltung =>
+        veranstaltung.name.toLowerCase().includes(stichwort.toLowerCase())
+    );
+
+    if (gefundeneVeranstaltungen.length === 0) {
+        return res.json({ message: 'Keine Veranstaltungen gefunden' });
+    }
+
+    res.json(gefundeneVeranstaltungen);
+});
+
+
+ // Veranstaltung löschen
 
 app.delete('/api/veranstaltungen/:veranstaltungId', (req, res) => {
     const veranstaltungId = req.params.veranstaltungId;
     AlleVeranstaltungen = AlleVeranstaltungen.filter(veranstaltung => veranstaltung.id !== veranstaltungId);
     res.json(AlleVeranstaltungen)
 })
-
+// Port anzeige in Konsole
 app.listen(8000, ()=> {
     console.log('Server is listening on port 8000')
 });
