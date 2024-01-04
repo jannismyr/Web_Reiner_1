477b8cd16a3dc80a539eb6f04ec939cf9892b402
FassbinderK <116159998+FassbinderK@users.noreply.github.com>
2024-01-04 13:56:30 +0100

    Veranstaltungen haben einen Timestamp und werden sortiert ausgegeben.

--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -5,6 +5,7 @@ export const AlleVeranstaltungen = [{
     ort: 'Brunnenplatz',
     preis: '0,00',
     genehmigung: true,
+    Zeitstempel: "2023-12-04T12:36:56.908Z"
 }, {
     id: '2',
     name: 'Felix Rave',
@@ -12,6 +13,7 @@ export const AlleVeranstaltungen = [{
     ort: 'Rummel',
     preis: '24,00',
     genehmigung: true,
+    Zeitstempel: "2023-12-05T12:36:56.908Z"
 }, {
     id: '3',
     name: 'Reinorgeln',
@@ -19,4 +21,5 @@ export const AlleVeranstaltungen = [{
     ort: 'Simons Haus',
     preis: '0,00',
     genehmigung: false,
+    Zeitstempel: "2023-12-06T12:36:56.908Z"
 }];
